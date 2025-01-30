8284e8b40c0f7294c0ae484a20f302db1474c639
Anand P <anandpamegoze@gmail.com>
2025-01-30 11:56:21 +0530

    dark and light theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import ModeToggle from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 
 export default function Home() {
   return (
-    <div>
+    <div className="m-4">
       <SignedOut>
             <SignInButton mode="modal">
              <Button>Sign In</Button>
@@ -13,6 +14,8 @@ export default function Home() {
           <SignedIn>
             <UserButton />
           </SignedIn>
+          <ModeToggle/>
+          <Button variant={"secondary"}>Click Me</Button>
     </div>
   );
 }
